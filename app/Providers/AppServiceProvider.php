20893ae7cbd48c12e1efea7fbf31861831d4ec20
Satrio <56964640+satriowaskitho@users.noreply.github.com>
2025-10-28 17:21:38 +0700

    Force HTTPS in production environment
    Added logic to enforce HTTPS in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force HTTPS in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+
+            // Force HTTPS detection for signed URLs
+            request()->server->set('HTTPS', 'on');
+        }
     }
 }
